5ca523064ef0f3e09a448a381a7a85e6c0239251
justinbaskaran <justinbaskaran5@gmail.com>
2018-11-10 12:28:42 -0500

    GUIpart1finished

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 $(document).ready(function () {
 
-        // This is example code
-/* 
-    $("#heading").click(function () {
-        $('#heading').css('color', 'yellow');
-    });
-*/ 
-
-
-
-
 });
