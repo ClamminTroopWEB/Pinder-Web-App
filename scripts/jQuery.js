817163ee34a162c4cbb526d0cc893f5662a220f5
Gavin <Nicaderp@MacBook-Air-9.local>
2018-11-22 15:47:08 -0500

    individualizedScripts

--- scripts/jQuery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $(".matchRow").click(function(){
-        var clickedBttnElement = $(this);// or var clickedBtnID = this.id
-        alert('clickedBttnElement: ' + clickedBttnElement);
-        window.location.href='./matchProfile.html'
-    });
-
-});
