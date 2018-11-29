13bc6e98e94a22a96c6604b89d5c102c0649018d
justinbaskaran <justinbaskaran5@gmail.com>
2018-11-29 18:21:38 -0500

    removed_modules

--- public/scripts/matchProfileScript.js
+++
@@ -1,9 +0,0 @@
-/* matchesProfile.js
- *
- * Authors: Justin Baskaran, Gavin Martin, Ian Christensen
- * Professor: Keith Vander Linden
- * Class: CS-336-A, for Calvin College
- * Semester: Fall, 2018
- */
-
-"use_strict";
