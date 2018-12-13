70e03d4d754adcc29ef8f4ac4a19585e1e6d39d7
justinbaskaran <justinbaskaran5@gmail.com>
2018-12-13 17:36:38 -0500

    fixed selection thingy

--- public/scripts/Selection.js
+++ public/scripts/Selection.js
@@ -17,17 +17,24 @@ import List from './List.js';
 import Adopt from './Adopt.js';
 import DogBox from './DogBox.js';
 import Profile from './Profile.js';
-import c from '../styles/combined.css';
+import c from '../styles/style.css';
+
 
 module.exports = React.createClass({
   render: function() {
     return (
       <nav id={c.mainScreen}>
+
         <img id={c.pinderLogoMain} src="https://i.imgur.com/pbgkjCN.png"/>
-        <div id={c.bttnContainer}>
-          <Link to="/List"><button id={c.listPetBttn} className={c.blueBttn}>List Pet</button></Link>
+        <div id={c.bttnContainer} > 
+        
+          <Link to="/List">
+          <button id={c.listPetBttn} className={c.blueBttn}>List Pet</button>
+          </Link>
+      
           <Link to="/Adopt"><button id={c.adoptPetBttn} className={c.blueBttn}>Adopt a Pet</button></Link>
           <Link to="/Matches"><button id={c.yourMatchesBttn} className={c.blueBttn}>Your Matches</button></Link>
+      
         </div>
         <Link to="/Profile"><button id={c.myProfileBttn} className={c.smallBlueBttn} onClick={this.yourProfile}>My Profile</button></Link>
       </nav>
